a0c662d45bfce17f5ae4c85191d116e97460d6a3
Tony Liang <chunqingliang@gmail.com>
2023-04-13 00:14:48 -0700

    merge modelling scripts to same module

--- src/modelling.py
+++ src/modelling.py
@@ -1,5 +1,27 @@
 import pandas as pd
 from sklearn.model_selection import cross_val_score, cross_validate
+import numpy as np
+
+
+# implementation of OLS in data
+# known B = (XTX)^-1XT Y
+def OLS(X_mat, y_mat):
+    """
+    Converts the parameters to numpy arrays and perform matrix multiplication to get betas of OLS from
+    (X^TX)^-1 X^T y
+    """
+    # add intercept column to matrix X
+    X = X_mat
+    y = y_mat
+    try:
+        X.insert(0,'intercept',1)
+    except:
+        pass
+    X = X.to_numpy()
+    y = y.to_numpy()
+    beta = np.linalg.inv(X.T @ X) @ X.T @ y
+    return beta[0], beta[1:]
+
 
 # helper function to calculate mean and stf for cv scores
 def mean_std_cross_val_scores(model, X_train, y_train, **kwargs):
