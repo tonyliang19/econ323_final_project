d23b20357efe0d5525614581eef7610b68c1c779
Tony Liang <chunqingliang@gmail.com>
2023-04-18 23:41:22 -0700

    update split_data to handle more complex stuff

--- src/handle_data.py
+++ src/handle_data.py
@@ -31,7 +31,7 @@ def get_data(data_path):
 # loads data from path, and specifies proportion of train data, with 1 - proportion of test data
 # and target is the variable of interest (your y), then returns train and test data
 # proportion is default to 0.5, and target default to None 
-def split_data(data_path, proportion=0.5, target=None, random_state=123):
+def split_data(data_path, proportion=0.5, target=None, random_state=123, **kwargs):
     """
     Loads data from path, and specifies proportion of train data, with 1 - proportion of test data
     and target is the variable of interest (your y), then returns train and test data
@@ -45,7 +45,7 @@ def split_data(data_path, proportion=0.5, target=None, random_state=123):
     # drop nas
     data = data.dropna()
     # inner function to split data into train and test portion
-    def train_test_split(data, proportion):
+    def train_test_split(data, proportion, plain=True, chosen_cols=None):
         train = data.sample(frac = proportion, random_state=random_state)
         test = data.drop(train.index)
         # rest and remove index of both
@@ -55,18 +55,20 @@ def split_data(data_path, proportion=0.5, target=None, random_state=123):
         assert train.shape[0] + test.shape[0] == data.shape[0]
         return train, test
     # split the data into train and test
-    train, test = train_test_split(data, proportion)
+    train, test = train_test_split(data, proportion, **kwargs)
     # further split train data to X and y
-    def split_X_y(data):
-        X = data.drop(columns=[target])
+    def split_X_y(data, plain=True, chosen_cols=None):
+        if plain is not True:
+            X = data[chosen_cols]
+        else:
+            X = data.drop(columns=[target])
         y = data[target]
         return X, y
-    X_train, y_train = split_X_y(train)
+    X_train, y_train = split_X_y(train, **kwargs)
     # split test data to X and y
-    X_test, y_test = split_X_y(test)
+    X_test, y_test = split_X_y(test, **kwargs)
     # check dimension again
     assert X_train.shape[0] + X_test.shape[0] == data.shape[0]
-    assert X_train.shape[1] and X_test.shape[1] == 13
     assert y_train.shape[0] == X_train.shape[0] and y_test.shape[0] == X_test.shape[0]
     # return the objects needed
     return X_train, X_test, y_train, y_test
